41745f3f17e036a85d82d4e24176cabe9188bd3b
Kun Zheng <v_zhengkun@baidu.com>
2016-06-28 23:05:04 +0800

    update config

--- src/main/java/com/alibaba/middleware/race/RaceConfig.java
+++ src/main/java/com/alibaba/middleware/race/RaceConfig.java
@@ -9,21 +9,21 @@ public class RaceConfig implements Serializable {
     // 天猫每分钟的交易金额的key更新为platformTmall_TeamCode_整分时间戳,
     // 每整分时刻无线和PC端总交易金额比值的key 更新为ratio_TeamCode_整分时间戳，
     // TeamCode是每个队伍的唯一标识
-    public static String prex_tmall = "platformTmall_APU_";
-    public static String prex_taobao = "platformTaobao_APU";
-    public static String prex_ratio = "ratio_APU_";
+    public static String prex_tmall = "platformTmall_41413few7x_";
+    public static String prex_taobao = "platformTaobao_41413few7x_";
+    public static String prex_ratio = "ratio_41413few7x_";
 
 
     // 这些jstorm/rocketMq/tair 的集群配置信息，这些配置信息在正式提交代码前会被公布
-    public static String JstormTopologyName = "xxx";
-    public static String MetaConsumerGroup = "xxx";
+    public static String JstormTopologyName = "41413few7x";
+    public static String MetaConsumerGroup = "41413few7x";
     public static String MqPayTopic = "MiddlewareRaceTestData_Pay";
     public static String MqTmallTradeTopic = "MiddlewareRaceTestData_TMOrder";
     public static String MqTaobaoTradeTopic = "MiddlewareRaceTestData_TBOrder";
-    public static String TairConfigServer = "xxx";
-    public static String TairSalveConfigServer = "xxx";
-    public static String TairGroup = "xxx";
-    public static Integer TairNamespace = 1;
+    public static String TairConfigServer = "10.101.72.127:5198";
+    public static String TairSalveConfigServer = "10.101.72.128:5198";
+    public static String TairGroup = "group_tianchi";
+    public static Integer TairNamespace = 45596;
 
     // jstorm的拓扑流程中的个组件的ID统一在此处设置
     public static final String SPOUT_ID = "spout";
