bcb1e48a0e3794a1112e8c629ea737d4ae76da19
is908 <kunchen@pku.edu.cn>
2016-07-01 18:42:35 +0800

    完善jstorm的spout从rocketMQ中拉取数据

--- src/main/java/com/alibaba/middleware/race/jstorm/RaceEventSpout.java
+++ src/main/java/com/alibaba/middleware/race/jstorm/RaceEventSpout.java
@@ -1,8 +1,6 @@
 package com.alibaba.middleware.race.jstorm;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 
@@ -10,15 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.alibaba.middleware.race.RaceConfig;
-import com.alibaba.middleware.race.RaceUtils;
-import com.alibaba.middleware.race.model.OrderMessage;
 import com.alibaba.middleware.race.model.PaymentMessage;
+import com.alibaba.middleware.race.rocketmq.Consumer;
 import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
-import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
-import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
-import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
-import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
-import com.alibaba.rocketmq.common.message.MessageExt;
+import com.alibaba.rocketmq.client.exception.MQClientException;
 
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
@@ -30,55 +23,24 @@ import backtype.storm.tuple.Values;
 public class RaceEventSpout implements IRichSpout {
     private static Logger LOG = LoggerFactory.getLogger(RaceEventSpout.class);
 
-    SpoutOutputCollector _collector;
-    private static Random rand;
-    private OrderMessage orderMessage;
+    SpoutOutputCollector collector;
     private BlockingQueue<PaymentMessage> queue = new LinkedBlockingDeque<>();
+    private DefaultMQPushConsumer consumer;
 
     @Override
     public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
-        _collector = collector;
+        this.collector = collector;
         try {
-            DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("41413few7x");
-            consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-            // TODO 修改name server
-            consumer.setNamesrvAddr("127.0.0.1:9876");
-
-            consumer.subscribe(RaceConfig.MqPayTopic, "*");
-
-            consumer.registerMessageListener(new MessageListenerConcurrently() {
-                @Override
-                public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
-                                                                ConsumeConcurrentlyContext context) {
-                    for (MessageExt msg : msgs) {
-                        byte [] body = msg.getBody();
-                        if (body.length == 2 && body[0] == 0 && body[1] == 0) {
-                            //Info: 生产者停止生成数据, 并不意味着马上结束
-                            System.out.println("Got the end signal");
-                            continue;
-                        }
-                        PaymentMessage paymentMessage = RaceUtils.readKryoObject(PaymentMessage.class, body);
-                        queue.offer(paymentMessage);
-                        System.out.println(paymentMessage);
-                    }
-                    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
-                }
-            });
-
-            consumer.start();
-        } catch (Exception e) {
-
+            this.consumer = Consumer.getConsumer(this.queue);
+        } catch (MQClientException e) {
+            LOG.error(e.getMessage());
+            e.printStackTrace();
         }
 
     }
 
     @Override
     public void nextTuple() {
-//        orderMessage.setCreateTime(System.currentTimeMillis());
-//        PaymentMessage[] paymentMessages = PaymentMessage.createPayMentMsg(orderMessage);
-//        for (PaymentMessage tmp : paymentMessages) {
-//            this._collector.emit(new Values(tmp));
-//        }
         if (queue.isEmpty()) {
             try {
                 Thread.sleep(10);
@@ -88,7 +50,7 @@ public class RaceEventSpout implements IRichSpout {
         } else {
             PaymentMessage paymentMessage = queue.poll();
             if (paymentMessage != null) {
-                this._collector.emit(new Values(paymentMessage));
+                this.collector.emit(new Values(paymentMessage));
             }
         }
 
@@ -101,7 +63,8 @@ public class RaceEventSpout implements IRichSpout {
 
     @Override
     public void fail(Object id) {
-        _collector.emit(new Values(id), id);
+        consumer.shutdown();
+        collector.emit(new Values(id), id);
     }
 
     @Override
@@ -111,23 +74,21 @@ public class RaceEventSpout implements IRichSpout {
 
     @Override
     public void close() {
-
+        consumer.shutdown();
     }
 
     @Override
     public void activate() {
+        consumer.resume();
     }
 
     @Override
     public void deactivate() {
-
+        consumer.suspend();
     }
 
     @Override
     public Map<String, Object> getComponentConfiguration() {
-        this.rand = new Random();
-        int platform = rand.nextInt(2);
-        orderMessage = ( platform == 0 ? OrderMessage.createTbaoMessage() : OrderMessage.createTmallMessage());
         return null;
     }
 }
--- src/main/java/com/alibaba/middleware/race/rocketmq/Consumer.java
+++ src/main/java/com/alibaba/middleware/race/rocketmq/Consumer.java
@@ -1,6 +1,11 @@
 package com.alibaba.middleware.race.rocketmq;
 
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+
 import com.alibaba.middleware.race.RaceConfig;
+import com.alibaba.middleware.race.RaceUtils;
+import com.alibaba.middleware.race.model.PaymentMessage;
 import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
 import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
@@ -8,10 +13,6 @@ import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently
 import com.alibaba.rocketmq.client.exception.MQClientException;
 import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
 import com.alibaba.rocketmq.common.message.MessageExt;
-import com.alibaba.middleware.race.model.*;
-import com.alibaba.middleware.race.RaceUtils;
-
-import java.util.List;
 
 
 /**
@@ -22,6 +23,41 @@ import java.util.List;
  * RocketMq消费组信息我们都会再正式提交代码前告知选手
  */
 public class Consumer {
+
+    public static DefaultMQPushConsumer getConsumer(final BlockingQueue<PaymentMessage> queue) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(RaceConfig.MetaConsumerGroup);
+        /**
+         * 设置Consumer第一次启动是从队列头部开始消费还是队列尾部开始消费<br>
+         * 如果非第一次启动，那么按照上次消费的位置继续消费
+         */
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+        // TODO 在本地搭建好broker后,记得指定nameServer的地址
+        consumer.setNamesrvAddr("127.0.0.1:9876");
+        
+        consumer.subscribe(RaceConfig.MqPayTopic, "*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                for (MessageExt msg : msgs) {
+                    byte [] body = msg.getBody();
+                    if (body.length == 2 && body[0] == 0 && body[1] == 0) {
+                        //Info: 生产者停止生成数据, 并不意味着马上结束
+                        System.out.println("Got the end signal");
+                        continue;
+                    }
+                    PaymentMessage paymentMessage = RaceUtils.readKryoObject(PaymentMessage.class, body);
+                    queue.offer(paymentMessage);
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        System.out.println("Consumer Started.");
+        return consumer;
+    }
 
     public static void main(String[] args) throws InterruptedException, MQClientException {
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("41413few7x");
