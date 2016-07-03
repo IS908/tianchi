d115c65c9a6fb8e3894b8fcc452adcd712c91d0d
Kun Zheng <v_zhengkun@baidu.com>
2016-07-03 08:18:45 +0800

    将tair操作类更改为单例

--- src/main/java/com/alibaba/middleware/race/Tair/TairOperatorImpl.java
+++ src/main/java/com/alibaba/middleware/race/Tair/TairOperatorImpl.java
@@ -19,7 +19,9 @@ public class TairOperatorImpl {
     private DefaultTairManager tairManager;
     private int namespace;
 
-    public TairOperatorImpl(String masterConfigServer, String slaveConfigServer, String groupName, int namespace) {
+
+
+    private TairOperatorImpl(String masterConfigServer, String slaveConfigServer, String groupName, int namespace) {
         tairManager = new DefaultTairManager();
         List<String> configServerList = Arrays.asList(masterConfigServer, slaveConfigServer);
         tairManager.setConfigServerList(configServerList);
@@ -55,9 +57,7 @@ public class TairOperatorImpl {
 
     //天猫的分钟交易额写入tair
     public static void main(String[] args) throws Exception {
-        TairOperatorImpl tairOperator =
-                new TairOperatorImpl(RaceConfig.TairConfigServer, RaceConfig.TairSalveConfigServer,
-                                            RaceConfig.TairGroup, RaceConfig.TairNamespace);
+        TairOperatorImpl tairOperator = TairOperatorImpl.getInstance();
         //假设这是付款时间
         Long millisTime = System.currentTimeMillis();
         //由于整分时间戳是10位数，所以需要转换成整分时间戳
@@ -66,5 +66,16 @@ public class TairOperatorImpl {
         Double money = 100.0;
         //写入tair
         tairOperator.write(RaceConfig.prex_tmall + minuteTime, money);
+    }
+
+
+    private static class InstanceHolder {
+        static final TairOperatorImpl tairOperator =
+                new TairOperatorImpl(RaceConfig.TairConfigServer, RaceConfig.TairSalveConfigServer,
+                                            RaceConfig.TairGroup, RaceConfig.TairNamespace);
+    }
+
+    public static TairOperatorImpl getInstance() {
+        return InstanceHolder.tairOperator;
     }
 }
