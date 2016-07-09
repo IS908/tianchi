eb214a3ad1326eded74c4feb8105e4716ef376ea
Kun Zheng <v_zhengkun@baidu.com>
2016-07-09 17:50:23 +0800

    计算PC无线交易比率

--- src/main/java/com/alibaba/middleware/race/jstorm/BoltPayRatio.java
+++ src/main/java/com/alibaba/middleware/race/jstorm/BoltPayRatio.java
@@ -1,10 +1,13 @@
 package com.alibaba.middleware.race.jstorm;
 
+import backtype.storm.Constants;
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.IRichBolt;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Tuple;
+import clojure.lang.Atom;
+
 import com.alibaba.middleware.race.RaceConfig;
 import com.alibaba.middleware.race.RaceConstant;
 import com.alibaba.middleware.race.Tair.TairOperatorImpl;
@@ -24,6 +27,10 @@ public class BoltPayRatio implements IRichBolt {
 
     private Map<Long, AtomicDouble> wirelessMap = new HashMap<>();
     private Map<Long, AtomicDouble> pcMap = new HashMap<>();
+    private double lastTimestamp = -1;
+    private double maxTimestamp = -1;
+    private Set<Long> alterTimeSet = new HashSet<>();
+
 
     @Override
     public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
@@ -31,61 +38,81 @@ public class BoltPayRatio implements IRichBolt {
 
     @Override
     public void execute(Tuple tuple) {
+        String componentId = tuple.getSourceComponent();
         String streamId = tuple.getSourceStreamId();
-        if (streamId.equals(RaceConstant.STREAM_STOP)) {
-            LOG.info("<<< stop signal");
-            // 累加
-            List<Pair> pcList = new ArrayList<>();
-            for (Map.Entry<Long, AtomicDouble> entry : pcMap.entrySet()) {
-                pcList.add(new Pair(entry.getKey(), entry.getValue().doubleValue()));
-            }
-            Collections.sort(pcList);
 
-            Map<Long, Double> pcTotalMap = new HashMap<>();
-            for (int i = 0; i < pcList.size(); i++) {
-                if (i > 0) {
-                    pcList.get(i).price += pcList.get(i - 1).price;
+        if (RaceConstant.STREAM_STOP.equals(streamId)) {
+            // 停止信号
+            LOG.info("stop signal");
+        } else if (Constants.SYSTEM_COMPONENT_ID.equals(componentId) && Constants.SYSTEM_TICK_STREAM_ID.equals(streamId)){
+            // tick stream singal
+            // 开始写数据
+            if (!alterTimeSet.isEmpty()) {
+                for (Long time : alterTimeSet) {
+                    AtomicDouble wirelessPrice = wirelessMap.get(time);
+                    AtomicDouble pcPrice = pcMap.get(time);
+                    if (wirelessPrice != null && pcPrice != null) {
+                        double ratio = TableItemFactory.round(wirelessPrice.doubleValue() / pcPrice.doubleValue(), 2);
+                        TairOperatorImpl.getInstance().write(RaceConfig.prex_ratio + time, ratio);
+                    }
                 }
-                pcTotalMap.put(pcList.get(i).timestamp, pcList.get(i).price);
+                alterTimeSet.clear();
             }
 
-            List<Pair> wirelessList = new ArrayList<>();
-            for (Map.Entry<Long, AtomicDouble> entry : wirelessMap.entrySet()) {
-                wirelessList.add(new Pair(entry.getKey(), entry.getValue().doubleValue()));
-            }
-            Collections.sort(wirelessList);
-            for (int i = 1; i < wirelessList.size(); i++) {
-                wirelessList.get(i).price += wirelessList.get(i - 1).price;
-            }
-
-            for (Pair wireless : wirelessList) {
-                Double pcPrice = pcTotalMap.get(wireless.timestamp);
-                if (pcPrice != null) {
-                    String key = RaceConfig.prex_ratio + wireless.timestamp;
-                    double value = TableItemFactory.round(wireless.price / pcPrice, 2);
-//                    TairOperatorImpl.getInstance().write(key, value);
-                    LOG.info(">>> {}:{}", key, value);
-                }
-            }
-        } else if (streamId.equals(RaceConstant.STREAM_PAY_PLATFORM)) {
+        }
+        else if (streamId.equals(RaceConstant.STREAM_PAY_PLATFORM)) {
             //            long orderID = tuple.getLong(0);
             short platform = tuple.getShort(1);
             long timestamp = tuple.getLong(2);
             double price = tuple.getDouble(3);
             if (platform == 0) { // PC
-                AtomicDouble oldValue = pcMap.get(timestamp);
-                if (oldValue == null) {
-                    oldValue = new AtomicDouble(price);
+                if (timestamp < maxTimestamp) {
+                    // 该tuple的时间小于最大时间，出现了乱序
+                    // 可以忽略
+                } else if(timestamp == maxTimestamp) {
+                    // tuple的时间是正在处理的时间，该tuple属于当前的时间
+                    AtomicDouble oldValue = pcMap.get(timestamp);
+                    if (oldValue == null) {
+                        oldValue = new AtomicDouble(price);
+                    } else {
+                        oldValue.addAndGet(price);
+                    }
                     pcMap.put(timestamp, oldValue);
+                    alterTimeSet.add(timestamp);
                 } else {
-                    oldValue.addAndGet(price);
+                    // tuple的时间 大于最大时间，新的一分钟的数据出现了
+                    lastTimestamp = maxTimestamp;
+                    maxTimestamp = timestamp;
+                    AtomicDouble lastPrice = pcMap.get(lastTimestamp);
+                    if (lastPrice != null) {
+                        pcMap.put(timestamp, new AtomicDouble(price + lastPrice.doubleValue()));
+                    } else {
+                        pcMap.put(timestamp, new AtomicDouble(price));
+                    }
                 }
             } else { // 无线
-                AtomicDouble oldValue = wirelessMap.get(timestamp);
-                if (oldValue == null) {
-                    wirelessMap.put(timestamp, new AtomicDouble(price));
+                if (timestamp < maxTimestamp) {
+                    // 该tuple的时间小于最大时间
+                } else if(timestamp == maxTimestamp) {
+                    // tuple的时间是正在处理的时间，该tuple属于当前的时间
+                    AtomicDouble oldValue = wirelessMap.get(timestamp);
+                    if (oldValue == null) {
+                        oldValue = new AtomicDouble(price);
+                    } else {
+                        oldValue.addAndGet(price);
+                    }
+                    wirelessMap.put(timestamp, oldValue);
+                    alterTimeSet.add(timestamp);
                 } else {
-                    oldValue.addAndGet(price);
+                    // tuple的时间 大于最大时间，新的一分钟的数据出现了
+                    lastTimestamp = maxTimestamp;
+                    maxTimestamp = timestamp;
+                    AtomicDouble lastPrice = wirelessMap.get(lastTimestamp);
+                    if (lastPrice != null) {
+                        wirelessMap.put(timestamp, new AtomicDouble(price + lastPrice.doubleValue()));
+                    } else {
+                        wirelessMap.put(timestamp, new AtomicDouble(price));
+                    }
                 }
             }
         }
@@ -105,23 +132,4 @@ public class BoltPayRatio implements IRichBolt {
         return null;
     }
 
-    private class Pair implements Comparable<Pair> {
-        public long timestamp;
-        public double price;
-
-        public Pair(long timestamp, double price) {
-            this.timestamp = timestamp;
-            this.price = price;
-        }
-
-        @Override
-        public int compareTo(Pair o) {
-            if (timestamp < o.timestamp) {
-                return -1;
-            } else if (timestamp > o.timestamp) {
-                return 1;
-            }
-            return 0;
-        }
-    }
 }
