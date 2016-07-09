b48c396573561534cd8630470347877601d1de8a
ubuntuck <1196355049@qq.com>
2016-07-09 15:37:13 +0800

    单独跑天猫/淘宝 分平台统计提测一次

--- src/main/java/com/alibaba/middleware/race/jstorm/platform/BoltTMCount.java
+++ src/main/java/com/alibaba/middleware/race/jstorm/platform/BoltTMCount.java
@@ -9,7 +9,6 @@ import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Tuple;
 import com.alibaba.middleware.race.RaceConfig;
 import com.alibaba.middleware.race.RaceConstant;
-import com.alibaba.middleware.race.Tair.TairOperatorImpl;
 import com.google.common.util.concurrent.AtomicDouble;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,6 +25,8 @@ public class BoltTMCount implements IRichBolt {
 
     private Map<Long, AtomicDouble> tmMap = new HashMap<>();
     private long cur_timestamp = 0L;
+    private boolean flag = false;
+
     @Override
     public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
 
@@ -33,14 +34,19 @@ public class BoltTMCount implements IRichBolt {
 
     @Override
     public void execute(Tuple tuple) {
-        if (tuple.getSourceComponent().equals(Constants.SYSTEM_COMPONENT_ID)
+        if (flag
+                && tuple.getSourceComponent().equals(Constants.SYSTEM_COMPONENT_ID)
                 && tuple.getSourceStreamId().equals(Constants.SYSTEM_TICK_STREAM_ID)) {
+            // 系统计时信号，执行写tair操作
             write3Tair();
-        } else {
-            if (tuple.getSourceStreamId().equals(RaceConstant.STREAM_STOP)) {
-                write3Tair();
-                return;
-            }
+            flag = false;
+        } else if (tuple.getSourceComponent().equals(RaceConstant.ID_PAIR)
+                && tuple.getSourceStreamId().equals(RaceConstant.STREAM_STOP)) {
+            // 结束信号，执行写tair操作
+            write3Tair();
+        } else if (tuple.getSourceComponent().equals(RaceConstant.ID_PAIR)
+                && tuple.getSourceStreamId().equals(RaceConstant.STREAM_PLATFORM_TM)) {
+            // 正常处理逻辑
             long timestamp = tuple.getLongByField(RaceConstant.payTime);
             double price = tuple.getDoubleByField(RaceConstant.payAmount);
             cur_timestamp = Math.max(timestamp, cur_timestamp);
@@ -50,6 +56,7 @@ public class BoltTMCount implements IRichBolt {
             }
             total.addAndGet(price);
             tmMap.put(timestamp, total);
+            flag = true;
         }
     }
 
@@ -59,8 +66,9 @@ public class BoltTMCount implements IRichBolt {
         for (long timestamp = before; timestamp <= after; timestamp += 60) {
             AtomicDouble result = tmMap.get(timestamp);
             if (result != null) {
-                TairOperatorImpl.getInstance().write(
-                        RaceConfig.prex_taobao + timestamp, result.doubleValue());
+//                TairOperatorImpl.getInstance().write(
+//                        RaceConfig.prex_tmall + timestamp, result.doubleValue());
+                LOG.info(">>> {}:{}", RaceConfig.prex_tmall + timestamp, result.doubleValue());
             }
         }
     }
--- src/main/java/com/alibaba/middleware/race/rocketmq/Producer.java
+++ src/main/java/com/alibaba/middleware/race/rocketmq/Producer.java
@@ -19,7 +19,7 @@ import java.util.concurrent.Semaphore;
 public class Producer {
 
     private static Random rand = new Random();
-    private static int count = 300000;
+    private static int count = 3000;
 
     /**
      * 这是一个模拟堆积消息的程序，生成的消息模型和我们比赛的消息模型是一样的，
@@ -117,6 +117,6 @@ public class Producer {
         }
         Thread.sleep(1000);
 
-        //        producer.shutdown();
+        producer.shutdown();
     }
 }
